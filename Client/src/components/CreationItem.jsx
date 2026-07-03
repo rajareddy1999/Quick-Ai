a25df51c6a07893d930f63e1136cfd350070918f
rajareddy1999 <rajarajareddy1999@gmail.com>
2026-07-03 08:27:02 +0530

    Initial commit

--- Client/src/components/CreationItem.jsx
+++ Client/src/components/CreationItem.jsx
@@ -1,50 +1,11 @@
-import React, { useState } from "react";
-import Markdown from "react-markdown";
+import React from 'react'
 
-
-const CreationItem = ({ item }) => {
-  const [expanded, setExpanded] = useState(false);
-
+const Creationitem = () => {
   return (
-    <>
-      <div
-        onClick={() => setExpanded(!expanded)}
-        className="p-4 max-w-5xl text-sm bg-white border border-gray-200 rounded-lg cursor-pointer"
-      >
-        <div className="flex justify-between items-center gap-4">
-          <div>
-            <h2>{item.prompt}</h2>
-            <p className="text-gray-500">
-              {item.type} -{" "}
-              {new Date(item.created_at).toLocaleDateString()}
-            </p>
-          </div>
+    <div>
+      
+    </div>
+  )
+}
 
-          <button className="bg-[#eff6ff] border border-[#bfdbfe] text-[#1e40af] px-4 py-1 rounded-full">
-            {item.type}
-          </button>
-        </div>
-
-        {expanded && (
-          <div>
-            {item.type === "image" ? (
-              <img
-                src={item.content}
-                alt="image"
-                className="mt-3 w-full max-w-md "
-              />
-            ) : (
-              <div className="max-h-64 overflow-y-auto text-sm text-slate-700">
-                <div className='reset-tw'>
-                    <Markdown>{item.content}</Markdown>
-                </div>
-              </div>
-            )}
-          </div>
-        )}
-      </div>
-    </>
-  );
-};
-
-export default CreationItem;
+export default Creationitem
